447d2d22ef0897880b8ba9d3203e8ba33361ee72
Bendasch <benediktscherner@posteo.de>
2021-10-18 15:06:30 +0200

    some more stuff

--- src/library/game.rs
+++ src/library/game.rs
@@ -36,167 +36,39 @@ pub struct Move {
 
 impl Move {
 
+    pub fn piece(&self) -> &Piece {
+        &self.piece
+    }
+
+    pub fn start_field(&self) -> &Field {
+        &self.start_field
+    }
+
+    pub fn target_field(&self) -> &Field {
+        &self.target_field
+    }
+
+    // for now we assume the syntax "AABB", where
+    // AA are the indices of the source field (i.e., 52)
+    // BB are the indices of the target field (i.e., 54)
+    // -> 5254 is equivalent to "e4" in standard notation.
     fn new(move_string: &mut str, game: &Game) -> Move {
         
         // transform to vector of characters and strip newlines or carriage returns
-        let mut move_chars: Vec<char> = move_string.chars().rev().filter(
+        let mut move_chars: Vec<char> = move_string.chars().filter(
             |c| *c != '\n' && *c != '\r'
         ).collect();
 
-        // (1) the last two chars are interpreted as the target field
-        let target_field = Move::get_target_field(&mut move_chars);
-        
-        // two characters at the most can be left,
-        // otherwise the move is not valid!
-        assert!(move_chars.len() <= 2);
-        
-        // (2) get the piece that was moved (incl. descriminator) 
-        let (piece, specifier) = Move::get_piece(&mut move_chars, &game.turn);
+        // get the piece from the poisition matrix
 
-        let start_field = Move::get_start_field(
-            specifier,
-            &piece,
-            &target_field,
-            game
-        );
-        
-        Move { 
-            piece: piece, 
-            start_field: start_field,
-            target_field: target_field 
+
+        // 
+        Move {
+            piece: 
+            start_field: move_chars[0..1]
+            target_field: move_chars[2..3]
         }
     }
-
-    fn get_target_field(move_chars: &mut Vec<char>) -> Field {
-        
-        let file = match move_chars.remove(0).to_digit(10) {
-            Some(rank) =>  rank as usize,
-            None => panic!("
-            Invalid move!")
-        };
-
-        let rank= match move_chars.remove(0) {
-            'a' => 1,
-            'b' => 2,
-            'c' => 3,
-            'd' => 4,
-            'e' => 5,
-            'f' => 6,
-            'g' => 7,
-            'h' => 8,
-            _ => panic!("Invalid move!")
-        };
-
-        Field(rank, file) 
-    }
-
-    fn get_start_field(
-        specifier: Option<char>, 
-        piece: &Piece, 
-        target_field: &Field,
-        game: &Game, 
-    ) -> Field {
-        
-        let field: Field; 
-        
-        // (1) get all remaining instances of >piece<
-        let instances = Move::get_piece_instances(piece, game);
-
-        // (2) check which instances can potentially reach >target_field<
-        let valid_instances = instances.iter().filter(|piece| {
-            Move::can_reach(piece, target_field, game.position_matrix().as_ref().unwrap())
-        }).collect::<Vec<_>>();
-
-        // (3) if multiple, check whether the specifier removes ambiguity
-        match valid_instances.len() {
-            0 => panic!("No movable piece could be identified - invalid move?!"),
-            1 => *valid_instances[0].field(),
-            _ => Move::determine_field_by_specifier(valid_instances, specifier)
-        }
-    }
-
-    fn get_piece_instances(piece: &Piece, game: &Game) -> Vec<PieceInstance>
-    {
-        let mut instances: Vec<PieceInstance> = Vec::new();
-        let matrix = game.position_matrix().as_ref().unwrap();
-        let mut rank_index: usize = 0;
-        for rank in matrix {
-            rank_index += 1;
-            for file_index  in (0..7) {
-                if rank[file_index] == *piece {
-                    instances.push(PieceInstance{ 
-                        piece: *piece,
-                        field: Field(rank_index, file_index)
-                    })
-                }               
-            }
-        }
-        instances
-    }
-
-    fn get_piece(move_chars: &mut Vec<char>, turn: &Color) -> (Piece, Option<char>) {
-        
-        // if no chars are left, the piece has to be a pawn
-        if move_chars.len() == 0 {
-            return (Piece{color: *turn, piecetype: PieceType::Pawn}, None)
-        } 
-
-        // first, pop the specifier off the array
-        let mut specifier   = None;
-        if move_chars.len() == 2 {
-            specifier = Some(move_chars.remove(0));
-        }
-
-        // otherwise we expect the piece to be contained
-        // in the remaining move chars
-        match move_chars.remove(0) {
-            'p' => return(Piece{color: *turn, piecetype: PieceType::Pawn}, specifier),
-            'r' => return(Piece{color: *turn, piecetype: PieceType::Rook}, specifier),
-            'n' => return(Piece{color: *turn, piecetype: PieceType::Knight}, specifier),
-            'b' => return(Piece{color: *turn, piecetype: PieceType::Bishop}, specifier),
-            'q' => return(Piece{color: *turn, piecetype: PieceType::Queen}, specifier),
-            'k' => return(Piece{color: *turn, piecetype: PieceType::King}, specifier),
-            _ => panic!("Invalid move!")  
-        }
-    }
-
-    fn can_reach(
-        pieceInstance: &PieceInstance, 
-        target_field: &Field,
-        position_matrix: &Vec<Vec<Piece>>
-    ) -> bool {
-        let &piece = pieceInstance.piece();
-        match piece.piecetype() {
-            PieceType::Rook    => Move::rook_can_reach(piece.color(), target_field, position_matrix),
-            PieceType::Knight  => Move::knight_can_reach(piece.color(), target_field, position_matrix),
-            PieceType::Bishop  => Move::bishop_can_reach(piece.color(), target_field, position_matrix),
-            PieceType::Queen   => Move::queen_can_reach(piece.color(), target_field, position_matrix),
-            PieceType::King    => Move::king_can_reach(piece.color(), target_field, position_matrix),
-            PieceType::Pawn    => Move::pawn_can_reach(piece.color(), target_field, position_matrix),
-            PieceType::None    => false
-        }
-    }
-
-    fn determine_field_by_specifier(valid_instances: Vec<&PieceInstance>, specifier: Option<char>) -> Field {
-        
-        let field: Field;
-
-        let specifier = match specifier {
-            Some(char) => char,
-            None => panic!("No specifier was provided, move ambiguous!")
-        };
-
-
-        field
-    }
-
-    fn rook_can_reach(color: &Color, target_field: &Field, position_matrix: &Vec<Vec<Piece>>) -> bool {true}
-    fn knight_can_reach(color: &Color, target_field: &Field, position_matrix: &Vec<Vec<Piece>>) -> bool {true}
-    fn bishop_can_reach(color: &Color, target_field: &Field, position_matrix: &Vec<Vec<Piece>>) -> bool {true}
-    fn queen_can_reach(color: &Color, target_field: &Field, position_matrix: &Vec<Vec<Piece>>) -> bool {true} 
-    fn king_can_reach(color: &Color, target_field: &Field, position_matrix: &Vec<Vec<Piece>>) -> bool {true}
-    fn pawn_can_reach(color: &Color, target_field: &Field, position_matrix: &Vec<Vec<Piece>>) -> bool {true}
-     
 }
 
 #[derive(PartialEq, Copy, Clone)]
@@ -231,9 +103,29 @@ impl<'a> Position<'a> {
         self.0.split("/") 
     }
 }
+
+pub struct PositionMatrix(Option<Vec<Vec<Piece>>>);
+
+impl PositionMatrix {
+    
+    fn has_piece_on_field(&self, field: &Field) -> bool {
+        match self.0 {
+            Some(matrix) => *matrix[field.0][field.1].piecetype() != PieceType::None, 
+            None => panic!("The matrix should always be prepared at this point!") 
+        }
+    }
+
+    fn get_piece_from_field(&self, field: &Field) -> &Piece {
+        match self.0 {
+            Some(matrix) => &matrix[field.0][field.1], 
+            None => panic!("The matrix should always be prepared at this point!") 
+        }
+    }
+}
+
 pub struct Game<'a> {
     position: Position<'a>,
-    position_matrix: Option<Vec<Vec<Piece>>>,
+    position_matrix: PositionMatrix,
     turn: Color,
     castle_availability: CastleAvailability,
     en_passant: Option<Field>,
@@ -263,14 +155,14 @@ impl<'a> Game<'a> {
         self.position
     }
 
-    pub fn position_matrix(&self) -> &Option<Vec<Vec<Piece>>> {
+    pub fn position_matrix(&self) -> &PositionMatrix {
         match self.position_matrix {
             Some(_) => &self.position_matrix,
             None => panic!("position matrix should have been buffered!")
         }
     }
 
-    fn buffer_matrix(&mut self) -> &Option<Vec<Vec<Piece>>> {
+    fn buffer_matrix(&mut self) -> &PositionMatrix {
         let mut matrix: Vec<Vec<Piece>> = Vec::new();
         let ranks = self.position.split();
         for (i, rank) in ranks.enumerate() {
@@ -296,7 +188,7 @@ impl<'a> Game<'a> {
             assert!(matrix[i].len() == 8, "Position matrix doesn't have 8 files");
         }
         assert!(matrix.len() == 8, "Position matrix doesn't have 8 ranks");
-        self.position_matrix = Some(matrix);
+        self.position_matrix = PositionMatrix(Some(matrix));
         &self.position_matrix
     }
 
@@ -313,15 +205,32 @@ impl<'a> Game<'a> {
     }
     
     pub fn next_move(&mut self) {
+
+        // transform the position string into a matrix 
+        self.buffer_matrix();
+        
+        // get the player input
         let mut move_string = String::new();
         io::stdin().read_line(&mut move_string).unwrap();
 
-        self.buffer_matrix();
+        // execute the players move
         let chess_move = Move::new(&mut move_string, &self);
-        self.make_move(chess_move);
+        self.execute_move(chess_move);
     }
 
-    fn make_move(&mut self, chess_move: Move) {
-        println!("About to move the {} from {} to {}.", chess_move.piece, chess_move.start_field, chess_move.target_field);
+    fn execute_move(&self, chess_move: Move) {
+
+        // (1) destroy enemy piece on the target field
+        // [note that we assume the move to be valid at this point]
+        if self.piece_on_field(chess_move.target_field()) {
+            println!("Hi, there is a piece on this field!")
+        }
+
+        // (2) place the piece on the field
+
+    }
+
+    fn piece_on_field(&self, field: &Field) -> bool {
+        self.position_matrix().has_piece_on_field(field)
     }
 }
