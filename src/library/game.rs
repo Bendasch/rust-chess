9c15e3d6234fe15435be9c4c2a23fd3dc2d57f7d
Bendasch <benediktscherner@posteo.de>
2021-10-20 09:00:30 +0200

    Internal mutability for position matrix

--- src/library/game.rs
+++ src/library/game.rs
@@ -1,6 +1,7 @@
 #![allow(dead_code)]
 use std::io;
 use std::cmp::PartialEq;
+use std::cell::RefCell;
 
 #[derive(PartialEq, Copy, Clone, Debug)]
 pub enum Color {
@@ -63,9 +64,9 @@ impl Move {
         ).collect();
         
         let start_field = Field(move_indices[0]-1, move_indices[1]-1);
-        let piece = game.position_matrix().unwrap().get_piece_from_field(&start_field);
+        let piece = game.position_matrix().borrow().get_piece_from_field(&start_field);
         Move {
-            piece: *piece,
+            piece: piece,
             start_field: start_field,
             target_field: Field(move_indices[2]-1, move_indices[3]-1)
         }
@@ -103,6 +104,10 @@ impl<'a> Position<'a> {
     pub fn split(&'a self) -> Vec<&'a str> {
         self.0.split("/").collect()
     }
+
+    pub fn update_from_matrix(&mut self) {
+        // TO DO!
+    }
 }
 
 #[derive(Clone)]
@@ -114,14 +119,22 @@ impl PositionMatrix {
         *self.0[field.0][field.1].piecetype() != PieceType::None
     }
 
-    fn get_piece_from_field(&self, field: &Field) -> &Piece {
-        &self.0[field.0][field.1]
+    fn get_piece_from_field(&self, field: &Field) -> Piece {
+        self.0[field.0][field.1]
+    }
+
+    fn empty_field(&mut self, field: Field) {
+        self.0[field.0][field.1] = Piece{color: Color::None, piecetype: PieceType::None};
+    }
+
+    fn place_piece(&mut self, piece: Piece, field: Field) {
+        self.0[field.0][field.1] = piece;
     }
 }
 
 pub struct Game<'a> {
     position: Position<'a>,
-    position_matrix: Option<PositionMatrix>,
+    position_matrix:  RefCell<PositionMatrix>,
     turn: Color,
     castle_availability: CastleAvailability,
     en_passant: Option<Field>,
@@ -131,9 +144,10 @@ pub struct Game<'a> {
 
 impl<'a> Game<'a> {
     pub fn new() -> Game<'a> {
+        let start_position = Position("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR");
         Game {
-            position: Position("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"),
-            position_matrix: None,
+            position: start_position,
+            position_matrix: Game::init_matrix(&start_position),
             turn: Color::White,
             castle_availability: CastleAvailability { 
                 white_king: true, 
@@ -151,16 +165,13 @@ impl<'a> Game<'a> {
         self.position
     }
 
-    pub fn position_matrix(&mut self) -> &Option<PositionMatrix> {
-        match self.position_matrix {
-            Some(_) => &self.position_matrix,
-            None => self.buffer_matrix()
-        }
+    pub fn position_matrix(&self) -> &RefCell<PositionMatrix> {
+        &self.position_matrix
     }
 
-    fn buffer_matrix(&mut self) -> &Option<PositionMatrix> {
+    fn init_matrix(start_position: &Position) -> RefCell<PositionMatrix> {
         let mut matrix: Vec<Vec<Piece>> = Vec::new();
-        let ranks: Vec<&str> = self.position.split();
+        let ranks: Vec<&str> = start_position.split();
         for (i, rank) in ranks.iter().rev().enumerate() {
             matrix.push(Vec::new());
             for char in rank.chars() {
@@ -185,8 +196,7 @@ impl<'a> Game<'a> {
             assert!(matrix[i].len() == 8, "Position matrix doesn't have 8 files\n{:?}", matrix[i]);
         }
         assert!(matrix.len() == 8, "Position matrix doesn't have 8 ranks");
-        self.position_matrix = Some(PositionMatrix(matrix));
-        &self.position_matrix
+        RefCell::new(PositionMatrix(matrix))
     }
 
     fn push_none(rank: &mut Vec<Piece>, num: usize) {
@@ -204,29 +214,110 @@ impl<'a> Game<'a> {
     }
     
     pub fn next_move(&mut self) {
-
-        // transform the position string into a matrix 
-        self.buffer_matrix();
-        
+       
         // get the player input
         let mut move_string = String::new();
         io::stdin().read_line(&mut move_string).unwrap();
 
         // execute the players move
-        let chess_move = Move::new(&mut move_string, &mut self);
+        let chess_move = Move::new(&mut move_string, self);
         self.execute_move(chess_move);
+
+        // parse position string
+        self.position.update_from_matrix();
     }
 
     fn execute_move(&mut self, chess_move: Move) {
 
         // 1) remove the piece from it's current field
-        let mut matrix = self.position_matrix().unwrap();
-
-
+        self.position_matrix().borrow_mut().empty_field(chess_move.start_field);
+    
         // 2) replace the new field with the piece
+        self.position_matrix().borrow_mut().place_piece(chess_move.piece, chess_move.target_field);
     }
 
     fn piece_on_field(&self, field: &Field) -> bool {
-        self.position_matrix().unwrap().has_piece_on_field(field)
+        self.position_matrix().borrow().has_piece_on_field(field)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+    
+    #[test]
+    fn new_game() {
+        let game = Game::new();
+        assert_eq!(game.position().0, "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR");
+        assert!(game.position_matrix().borrow().0[0][0] == Piece{color: Color::White, piecetype: PieceType::Rook});
+        assert!(game.position_matrix().borrow().0[0][1] == Piece{color: Color::White, piecetype: PieceType::Knight});
+        assert!(game.position_matrix().borrow().0[0][2] == Piece{color: Color::White, piecetype: PieceType::Bishop});
+        assert!(game.position_matrix().borrow().0[0][3] == Piece{color: Color::White, piecetype: PieceType::Queen});
+        assert!(game.position_matrix().borrow().0[0][4] == Piece{color: Color::White, piecetype: PieceType::King});
+        assert!(game.position_matrix().borrow().0[0][5] == Piece{color: Color::White, piecetype: PieceType::Bishop});
+        assert!(game.position_matrix().borrow().0[0][6] == Piece{color: Color::White, piecetype: PieceType::Knight});
+        assert!(game.position_matrix().borrow().0[0][7] == Piece{color: Color::White, piecetype: PieceType::Rook});
+        assert!(game.position_matrix().borrow().0[1][0] == Piece{color: Color::White, piecetype: PieceType::Pawn});
+        assert!(game.position_matrix().borrow().0[1][1] == Piece{color: Color::White, piecetype: PieceType::Pawn});
+        assert!(game.position_matrix().borrow().0[1][2] == Piece{color: Color::White, piecetype: PieceType::Pawn});
+        assert!(game.position_matrix().borrow().0[1][3] == Piece{color: Color::White, piecetype: PieceType::Pawn});
+        assert!(game.position_matrix().borrow().0[1][4] == Piece{color: Color::White, piecetype: PieceType::Pawn});
+        assert!(game.position_matrix().borrow().0[1][5] == Piece{color: Color::White, piecetype: PieceType::Pawn});
+        assert!(game.position_matrix().borrow().0[1][6] == Piece{color: Color::White, piecetype: PieceType::Pawn});
+        assert!(game.position_matrix().borrow().0[1][7] == Piece{color: Color::White, piecetype: PieceType::Pawn});        
+        assert!(game.position_matrix().borrow().0[6][0] == Piece{color: Color::Black, piecetype: PieceType::Pawn});
+        assert!(game.position_matrix().borrow().0[6][1] == Piece{color: Color::Black, piecetype: PieceType::Pawn});
+        assert!(game.position_matrix().borrow().0[6][2] == Piece{color: Color::Black, piecetype: PieceType::Pawn});
+        assert!(game.position_matrix().borrow().0[6][3] == Piece{color: Color::Black, piecetype: PieceType::Pawn});
+        assert!(game.position_matrix().borrow().0[6][4] == Piece{color: Color::Black, piecetype: PieceType::Pawn});
+        assert!(game.position_matrix().borrow().0[6][5] == Piece{color: Color::Black, piecetype: PieceType::Pawn});
+        assert!(game.position_matrix().borrow().0[6][6] == Piece{color: Color::Black, piecetype: PieceType::Pawn});
+        assert!(game.position_matrix().borrow().0[6][7] == Piece{color: Color::Black, piecetype: PieceType::Pawn});        
+        assert!(game.position_matrix().borrow().0[7][0] == Piece{color: Color::Black, piecetype: PieceType::Rook});
+        assert!(game.position_matrix().borrow().0[7][1] == Piece{color: Color::Black, piecetype: PieceType::Knight});
+        assert!(game.position_matrix().borrow().0[7][2] == Piece{color: Color::Black, piecetype: PieceType::Bishop});
+        assert!(game.position_matrix().borrow().0[7][3] == Piece{color: Color::Black, piecetype: PieceType::Queen});
+        assert!(game.position_matrix().borrow().0[7][4] == Piece{color: Color::Black, piecetype: PieceType::King});
+        assert!(game.position_matrix().borrow().0[7][5] == Piece{color: Color::Black, piecetype: PieceType::Bishop});
+        assert!(game.position_matrix().borrow().0[7][6] == Piece{color: Color::Black, piecetype: PieceType::Knight});
+        assert!(game.position_matrix().borrow().0[7][7] == Piece{color: Color::Black, piecetype: PieceType::Rook});
+    }
+
+    #[test]
+    fn matrix_empty_field() {
+        let game = Game::new();
+        assert!(game.piece_on_field(&Field(0,0)));
+        game.position_matrix().borrow_mut().empty_field(Field(0,0));
+        assert!(!game.piece_on_field(&Field(0,0)));
+    }
+
+    #[test]
+    fn new_move() {
+        let mut game = Game::new();
+        let mut move_string = String::from("2545");
+        let chess_move = Move::new(&mut move_string, &mut game);
+        assert_eq!(chess_move.piece, Piece{color: Color::White, piecetype: PieceType::Pawn});
+        assert_eq!(chess_move.start_field, Field(1,4));
+        assert_eq!(chess_move.target_field, Field(3,4));
+
+        let mut move_string = String::from("7363");
+        let chess_move = Move::new(&mut move_string, &mut game);
+        assert_eq!(chess_move.piece, Piece{color: Color::Black, piecetype: PieceType::Pawn});
+        assert_eq!(chess_move.start_field, Field(6,2));
+        assert_eq!(chess_move.target_field, Field(5,2));
+    }
+
+    #[test]
+    fn matrix_execute_move() {
+
+        let mut game = Game::new();
+        let mut move_string = String::from("7363");
+        let chess_move = Move::new(&mut move_string, &mut game);
+        let mut move_string = String::from("7363");
+        let chess_move_check = Move::new(&mut move_string, &mut game);
+
+        game.execute_move(chess_move);
+        assert_eq!(game.position_matrix().borrow().0[chess_move_check.target_field.0][chess_move_check.target_field.1], chess_move_check.piece);
     }
 }
