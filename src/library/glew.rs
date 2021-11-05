ce448ab9278c87f37ff2d6c3654f313090b6bce9
Bendasch <benediktscherner@posteo.de>
2021-11-06 00:53:58 +0100

    Abandoned glew. Pointers directly from wgl.

--- src/library/glew.rs
+++
@@ -1,7 +0,0 @@
-use libc::{c_int, c_uint, c_char, c_uchar};
-
-#[link(name = "glew32s", kind="static")]
-extern "C" {
-    pub fn glewInit() -> c_int;
-    //pub fn glGetString (name: c_int) -> *const c_char;
-}
