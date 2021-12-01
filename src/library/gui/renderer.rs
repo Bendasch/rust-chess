2a0269143018d1dde0b44f1617c680caa5feddd8
Bendasch <benediktscherner@posteo.de>
2021-12-01 17:50:32 +0100

    GUI update

--- src/library/gui/renderer.rs
+++ src/library/gui/renderer.rs
@@ -53,21 +53,33 @@ impl Renderer {
         let gl = Rc::new(GL::bind());
 
         // vertex buffer
-        let v_buffer = VertexBuffer::new(452, Rc::clone(&gl));
+        // 324 (tiles)
+        // 128 (pieces)
+        // 8 (UI)
+        let v_buffer = VertexBuffer::new(460, Rc::clone(&gl));
         let mut layout = VertexBufferLayout::new(v_buffer.buffer_id);
         layout.push::<f32>(2);
         layout.push::<f32>(2);
         layout.push::<f32>(1);
+
+        // we can bind the UI once at the start
+        let ui_verts = Renderer::get_ui_vertices();
+        v_buffer.buffer_sub_data(&ui_verts, ui_verts.len(), 452);
 
         // vertex array
         let mut v_array = VertexArray::new(Rc::clone(&gl));
         v_array.add_buffer(&v_buffer, &layout);
 
         // indices
+        // 384 (tiles)
+        // 192 (pieces)
+        // 12 (UI)
         let board_indices = Renderer::get_board_indices();
-        let i_buffer = IndexBuffer::new(576, Rc::clone(&gl));
+        let i_buffer = IndexBuffer::new(588, Rc::clone(&gl));
         i_buffer.bind();
         i_buffer.buffer_sub_data(&board_indices, board_indices.len(), 0);
+        let ui_indices = Renderer::get_ui_indices(452);
+        i_buffer.buffer_sub_data(&ui_indices, ui_indices.len(), 576);
 
         // shader
         let mut shader = Shader::new(
@@ -80,7 +92,8 @@ impl Renderer {
         let white_field = Texture::new("./src/library/gui/res/img/white_field.png", Rc::clone(&gl));
         let black_field = Texture::new("./src/library/gui/res/img/black_field.png", Rc::clone(&gl));
         let pieces = Texture::new("./src/library/gui/res/img/pieces.png", Rc::clone(&gl));
-        shader.set_uniform_1iv("u_Textures", vec![0, 1, 2]);
+        let ui_icons = Texture::new("./src/library/gui/res/img/ui_icons.png", Rc::clone(&gl));
+        shader.set_uniform_1iv("u_Textures", vec![0, 1, 2, 3]);
 
         Renderer::set_blend_func(Rc::clone(&gl));
 
@@ -100,7 +113,7 @@ impl Renderer {
             v_buffer,
             i_buffer,
             shader,
-            textures: vec![white_field, black_field, pieces],
+            textures: vec![white_field, black_field, pieces, ui_icons],
         }
     }
 
@@ -174,6 +187,8 @@ impl Renderer {
         let mvp = ortho(0.0, 1.0, 0.0, 1.0, -0.5, 0.5) * Renderer::get_board_transformation();
         self.shader.bind();
         self.shader.set_uniform_mat4f("u_MVP", mvp);
+        self.shader
+            .set_uniform_mat4f("u_MVP_UI", ortho(0.0, 1.0, 0.0, 1.0, -0.5, 0.5));
         gl!(self.gl.viewport(0, 0, WIDTH as i32, HEIGHT as i32));
     }
 
@@ -219,9 +234,7 @@ impl Renderer {
         for i in 0..=8 {
             for j in 0..=8 {
                 for k in 1..=4 {
-                    unsafe {
-                        vertices.push(Renderer::get_vertex(i, j, k));
-                    }
+                    vertices.push(Renderer::get_vertex(i, j, k));
                 }
             }
         }
@@ -229,23 +242,54 @@ impl Renderer {
         if let Some((x, y)) = *selected_field {
             let selected_field_indices = Renderer::get_board_indices_for_field(x, y);
             for index in selected_field_indices {
-                vertices[index].texture_id = 3.0;
+                vertices[index].texture_id = 9.0;
             }
         };
 
         Renderer::deserialize(vertices)
     }
 
-    unsafe fn get_vertex(rank: i32, file: i32, corner: i32) -> Vertex {
+    #[rustfmt::skip]
+    pub fn get_ui_vertices() -> Vec<f32> {
+        // one button should be 8x8% of the screen
+        let base_position = (0.75, 0.42);
+        let mut vertices = Vec::new();
+        for i in 0..=1 {
+            for j in 1..=4 {
+                let position = match j {
+                    1 => Position(base_position.0 + 0.12 * i as f32,        base_position.1),
+                    2 => Position(base_position.0 + 0.12 * i as f32 + 0.08, base_position.1),
+                    3 => Position(base_position.0 + 0.12 * i as f32 + 0.08, base_position.1 + 0.16),
+                    4 => Position(base_position.0 + 0.12 * i as f32,        base_position.1 + 0.16),
+                    _ => panic!("Invalid corner index"),
+                };
+                let texture_coords = Renderer::get_texture_coords(j, 2., i as f32, 1., 0.);
+                vertices.push(Vertex {
+                    position,
+                    texture_coords,
+                    texture_id: 3.0,
+                });
+            }
+        }
+
+        Renderer::deserialize(vertices)
+    }
+
+    #[rustfmt::skip]
+    fn get_texture_coords(corner: i32, num_cols: f32, col_idx: f32, num_rows: f32, row_idx: f32) -> TextureCoords {
+        match corner {
+            1 => TextureCoords(col_idx      / num_cols, row_idx     / num_rows),
+            2 => TextureCoords((col_idx+1.) / num_cols, row_idx     / num_rows),
+            3 => TextureCoords((col_idx+1.) / num_cols, (row_idx+1.) / num_rows),
+            4 => TextureCoords(col_idx      / num_cols, (row_idx+1.) / num_rows),
+            _ => panic!("Invalid corner index"),
+        }
+    }
+
+    fn get_vertex(rank: i32, file: i32, corner: i32) -> Vertex {
         let position = Position(rank as f32 * 1.0 / 8.0, file as f32 * 1.0 / 8.0);
 
-        let texture_coords = match corner {
-            1 => TextureCoords(0.0, 0.0),
-            2 => TextureCoords(1.0, 0.0),
-            3 => TextureCoords(1.0, 1.0),
-            4 => TextureCoords(0.0, 1.0),
-            _ => panic!("Invalid corner index"),
-        };
+        let texture_coords = Renderer::get_texture_coords(corner, 1., 0., 1., 0.);
 
         let texture_id = match corner {
             1 | 3 => ((rank + file + 1) % 2) as f32,
@@ -274,6 +318,23 @@ impl Renderer {
             }
         }
         indices
+    }
+
+    fn get_ui_indices(offset: u32) -> Vec<u32> {
+        vec![
+            offset,
+            offset + 1,
+            offset + 2,
+            offset + 2,
+            offset + 3,
+            offset,
+            offset + 4,
+            offset + 5,
+            offset + 6,
+            offset + 6,
+            offset + 7,
+            offset + 4,
+        ]
     }
 
     fn get_board_indices_for_field(x: usize, y: usize) -> Vec<usize> {
--- src/library/gui/res/simple.shader
+++ src/library/gui/res/simple.shader
@@ -5,17 +5,26 @@ layout(location = 0) in vec4 a_Position;
 layout(location = 1) in vec2 a_TexCoord;
 layout(location = 2) in float a_TexIndex;
 
-out vec4 v_Color;
 out vec2 v_TexCoord;    
 out float v_TexIndex;    
 
 uniform mat4 u_MVP;
+uniform mat4 u_MVP_UI;
 
 void main()
-{
-    gl_Position = u_MVP * a_Position;
-    v_TexCoord = a_TexCoord;
-    v_TexIndex = a_TexIndex;
+{   
+    if (a_TexIndex != 3.0)
+    {
+        gl_Position = u_MVP * a_Position;
+        v_TexCoord = a_TexCoord;
+        v_TexIndex = a_TexIndex;
+    }
+    else
+    {
+        gl_Position = u_MVP_UI * a_Position;
+        v_TexCoord = a_TexCoord;
+        v_TexIndex = a_TexIndex;
+    }
 }   
 
 #shader fragment
@@ -25,12 +34,12 @@ layout(location = 0) out vec4 o_Color;
 in vec2 v_TexCoord; 
 in float v_TexIndex; 
 
-uniform sampler2D u_Textures[3];
+uniform sampler2D u_Textures[4];
 
 void main()
 {
     int index = int(v_TexIndex);
-    if (index < 3.0)
+    if (index < 9.0)
     {
         o_Color = texture(u_Textures[index], v_TexCoord);
     }
